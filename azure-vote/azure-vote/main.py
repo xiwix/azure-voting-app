2b25e5fb39de4cace51cac612076e6fe377217cf
xiwix <fixme>
2020-12-08 09:29:43 -0700

    Added tests

--- azure-vote/azure-vote/main.py
+++ azure-vote/azure-vote/main.py
@@ -5,72 +5,47 @@ import random
 import socket
 import sys
 
-app = Flask(__name__)
+def create_app():
+    app = Flask(__name__)
 
-# Load configurations
-app.config.from_pyfile('config_file.cfg')
-button1 = app.config['VOTE1VALUE']
-button2 = app.config['VOTE2VALUE']
-title = app.config['TITLE']
+    # Load configurations
+    app.config.from_pyfile('config_file.cfg')
+    button1 = app.config['VOTE1VALUE']
+    button2 = app.config['VOTE2VALUE']
+    title = app.config['TITLE']
 
-# MySQL configurations
-app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
-app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
-app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE']
-app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_HOST']
+    app.config['env'] = os.getenv('env', 'unknown')
 
-# MySQL Object
-mysql = MySQL()
-mysql.init_app(app)
+    # MySQL configurations
+    app.config['MYSQL_DATABASE_USER'] = os.environ['MYSQL_USER']
+    app.config['MYSQL_DATABASE_PASSWORD'] = os.environ['MYSQL_PASSWORD']
+    app.config['MYSQL_DATABASE_DB'] = os.environ['MYSQL_DATABASE']
+    app.config['MYSQL_DATABASE_HOST'] = os.environ['MYSQL_HOST']
 
-# Change title to host name to demo NLB
-if app.config['SHOWHOST'] == "true":
-    title = socket.gethostname()
+    # MySQL Object
+    mysql = MySQL()
+    mysql.init_app(app)
+
+    # Change title to host name to demo NLB
+    if app.config['SHOWHOST'] == "true":
+        title = socket.gethostname()
 
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+    @app.route('/', methods=['GET', 'POST'])
+    def index():
 
-    # MySQL Connection
-    connection = mysql.connect()
-    cursor = connection.cursor()
+        if app.config['env'] == 'prod':
+            return '', 500
 
-    # Vote tracking
-    vote1 = 0
-    vote2 = 0
+        # MySQL Connection
+        connection = mysql.connect()
+        cursor = connection.cursor()
 
-    if request.method == 'GET':
+        # Vote tracking
+        vote1 = 0
+        vote2 = 0
 
-        # Get current values
-        cursor.execute('''Select votevalue, count(votevalue) as count From azurevote.azurevote
-        group by votevalue''')
-        results = cursor.fetchall()
-
-        # Parse results
-        for i in results:
-            if i[0] == app.config['VOTE1VALUE']:
-                vote1 = i[1]
-            elif i[0] == app.config['VOTE2VALUE']:
-                vote2 = i[1]
-
-        # Return index with values
-        return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
-
-    elif request.method == 'POST':
-
-        if request.form['vote'] == 'reset':
-
-            # Empty table and return results
-            cursor.execute('''Delete FROM azurevote''')
-            connection.commit()
-            return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
-        else:
-
-            # Insert vote result into DB
-            vote = request.form['vote']
-            cursor.execute(
-                '''INSERT INTO azurevote (votevalue) VALUES (%s)''', (vote))
-            connection.commit()
+        if request.method == 'GET':
 
             # Get current values
             cursor.execute('''Select votevalue, count(votevalue) as count From azurevote.azurevote
@@ -84,24 +59,61 @@ def index():
                 elif i[0] == app.config['VOTE2VALUE']:
                     vote2 = i[1]
 
-            # Return results
+            # Return index with values
             return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
 
+        elif request.method == 'POST':
 
-@app.route('/results')
-def results():
+            if request.form['vote'] == 'reset':
 
-    # MySQL Connection
-    connection = mysql.connect()
-    cursor = connection.cursor()
+                # Empty table and return results
+                cursor.execute('''Delete FROM azurevote''')
+                connection.commit()
+                return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
+            else:
 
-    # Get current values
-    cursor.execute('''Select * FROM azurevote''')
-    rv = cursor.fetchall()
-    return str(rv)
+                # Insert vote result into DB
+                vote = request.form['vote']
+                cursor.execute(
+                    '''INSERT INTO azurevote (votevalue) VALUES (%s)''', (vote))
+                connection.commit()
 
+                # Get current values
+                cursor.execute('''Select votevalue, count(votevalue) as count From azurevote.azurevote
+                group by votevalue''')
+                results = cursor.fetchall()
+
+                # Parse results
+                for i in results:
+                    if i[0] == app.config['VOTE1VALUE']:
+                        vote1 = i[1]
+                    elif i[0] == app.config['VOTE2VALUE']:
+                        vote2 = i[1]
+
+                # Return results
+                return render_template("index.html", value1=vote1, value2=vote2, button1=button1, button2=button2, title=title)
+
+    
+    @app.route('/health')
+    def health():
+        return 'UP'
+    
+    @app.route('/results')
+    def results():
+
+        # MySQL Connection
+        connection = mysql.connect()
+        cursor = connection.cursor()
+
+        # Get current values
+        cursor.execute('''Select * FROM azurevote''')
+        rv = cursor.fetchall()
+        return str(rv)
+
+    return app
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(host='0.0.0.0', debug=True, port=80)
-_ == "__main__":
-    app.run(host='0.0.0.0', debug=True, port=80)
+# _ == "__main__":
+    # app.run(host='0.0.0.0', debug=True, port=80)
